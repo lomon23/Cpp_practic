2e42ac8538f7b3c6c0f67cd89e57239774d70e9f
Your Name <you@example.com>
2025-09-10 09:29:46 +0300

    added calc v1

--- calculator_prjct/calculator_cpp.cpp
+++ calculator_prjct/calculator_cpp.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+using namespace std;
 
 int main(){
-    std::cout << "hello world" << std::endl;
+    double a, b;
+
+    cout << "введи перше число: ";
+    cin >> a;
+
+    cout << "Введи друге число: ";
+    cin >> b;
+
+    cout << "Сума: " << a + b << endl;
+    cout << "Різниця: " << a - b << endl;
+    cout << "Добуток: " << a * b << endl;
+    cout << "Ділення: " << a / b << endl;
+    
     return 0;
 }
